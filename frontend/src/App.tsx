6d341a2afb881171559aec9932d3c383308cd828
Hubert Leo <hubertleo@mac.mynetworksettings.com>
2025-09-25 19:18:29 -0400

    initial UI elements done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,14 +9,39 @@ function App() {
   return (
     <>
     <div className="w-screen min-h-screen bg-gray-200 font-sans flex flex-col">
-      <header className = "flex justify-center text-5xl">
-        Infinite Tic Tac Toe
+      <header className = "flex flex-col items-center">
+        <h1 className = "text-5xl">Infinite Tic Tac Toe</h1>
+        <div className = "flex gap-2">
+           <div id ="create-game">
+            <button className="border border-gray-300 px-5 py-3 rounded-2xl hover:cursor-pointer">
+              Create Game
+            </button>
+           </div>
+
+           <div id ="join-game" className = "bg-white w-100 rounded-xl flex items-center px-2 gap-4">
+
+
+
+            <input className = "w-50 border border-black rounded-lg" placeholder="GAME ID" />
+
+            <button className="border border-gray-300 px-2 py-1 rounded-lg">
+              JOIN
+              
+            </button>
+
+
+
+           </div>
+
+        </div>
       </header>
 
       <div className="flex justify-center items-center flex-1">
-        <div className="bg-gray-400 w-[400px] h-[400px]"></div>
+        <div className="grid grid-cols-3 gap-2"> 
+          {Array(9).fill("").map((elem) => (<div className="bg-gray-300 w-20 h-20
+           rounded-2xl border border-gray-400"></div>))}
+        </div>
       </div>
-
     </div>
       
     </>
